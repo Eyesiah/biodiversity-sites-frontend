7c86eba665d4f11d9744a9cf6428fda24fdd05d9
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-08-26 13:05:46 +0200

    correct setup for next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>BGS Register</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Biodiversity Gain Sites Register" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
