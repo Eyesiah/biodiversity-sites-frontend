8b6f736d075945b83ad015c7c4a52339860f2822
Mark CD Ashdown <markcdashdown@Marks-Mac-mini.fritz.box>
2025-09-02 14:55:20 +0100

    edit the About page and create an xml of responsible bodies

--- pages/about.js
+++ pages/about.js
@@ -12,6 +12,10 @@ export default function About() {
 
         <p>England's <ExternalLink href="https://www.google.com/search?q=Biodiversity+Net+Gain+(BNG)">Biodiversity Net Gain (BNG)</ExternalLink> requirement, which aims to leave nature in a measurably better state after a development project, means that developers must now achieve at least a 10% increase in the biodiversity of a parcel of land following its development. If this is not possible, Biodiversity Gain Sites (BGS) have been made available. These sites are locations where habitats for wildlife are created or improved. If biodiversity gains can’t be delivered within a development site - by creating or enhancing its existing habitats - they must be delivered elsewhere, by developers paying for the same types of habitat at a different location. Landowners have registered their land for this purpose and will sell <ExternalLink href="https://www.google.com/search?q=biodiversity+units">biodiversity units</ExternalLink> to developers.</p>
 
+        <h2>What is Biodiversity Net Gain?</h2>
+        <p>Biodiversity Net Gain (BNG) represents a transformative shift in England's approach to development and environmental policy. Enacted under the Environment Act 2021, BNG is a legally binding framework designed to ensure that new developments, with limited exemptions, leave habitats for wildlife in a measurably better state than they were before construction. This is quantified through the use of a statutory biodiversity metric, which assigns a numerical value, or ‘biodiversity unit’, to habitats. Developers must achieve a minimum 10% gain in these units, a commitment that must be legally secured for at least 30 years.</p>
+        <p>The BNG process is not simply a spreadsheet calculation; it is heavily dependent on the professional expertise and judgment of ecologists. Ecologists conduct a thorough assessment of a site's baseline conditions, which includes desk studies and field surveys to map and classify habitat types. They must also assess a site's strategic significance.</p>
+
         <h2>How we gathered the data</h2>
         <p>The data included in the list of biodiversity gain sites given here is compiled from various sources:</p>
         
@@ -22,8 +26,7 @@ export default function About() {
         <p><ExternalLink href="https://environment.data.gov.uk/biodiversity-net-gain/search">BGS Register Search</ExternalLink> This is the source of the base data. Until the data is provided publicly in computer-readable form, the data is extracted from the json files used to render the HTML website.</p>
 
         <h3>2. Responsible Bodies</h3>
-        <p>Details of the responsible bodies are taken from the <ExternalLink href="http://gov.uk">gov.uk</ExternalLink> document but the names do not agree with those used in the BGS Register.</p>
-        <p><ExternalLink href="https://www.gov.uk/government/publications/conservation-covenant-agreements-designated-responsible-bodies/conservation-covenants-list-of-designated-responsible-bodies">List of Designated Responsible Bodies</ExternalLink></p>
+        <p>Details of the responsible bodies are taken from the .GOV list <ExternalLink href="https://www.gov.uk/government/publications/conservation-covenant-agreements-designated-responsible-bodies/conservation-covenants-list-of-designated-responsible-bodies">Conservation covenants: list of designated responsible bodies</ExternalLink>.</p>
 
         <h3>3. Locations</h3>
         <p>Site locations are given but the precise location of allocations has to be deduced from the address using Google’s Geocoder. This sometimes doesn’t work well, so some checking is needed. If it fails, the location falls back to the LPA's centroid.</p>
@@ -42,10 +45,8 @@ export default function About() {
         <p>Baseline parcels are as per the standard formula – Habitat Unit (HU) = Habitat area/length (of parcel) x Distinctiveness (of Habitat) x Condition (of parcel) x Strategic Significance (where SS is set to Low, 1).</p>
 
         <h3>2. Improvements</h3>
-        <ul>
-          <li>Created parcels - HU = Habitat area/length (of parcel) x Distinctiveness (of Habitat) x Condition (of parcel) x Strategic Significance (of parcel – low, 1) x Temporal Risk (of Habitat and Condition) x Difficulty factor (of the Habitat) x Spatial Risk (Low, 1)</li>
-          <li>Enhanced parcels - HU is not computed because no connection can be made back to the baseline habitat parcel’s value.</li>
-        </ul>
+        <p>Created parcels - HU = Habitat area/length (of parcel) x Distinctiveness (of Habitat) x Condition (of parcel) x Strategic Significance (of parcel – low, 1) x Temporal Risk (of Habitat and Condition) x Difficulty factor (of the Habitat) x Spatial Risk (Low, 1)</p>
+        <p>Enhanced parcels - HU is not computed because no connection can be made back to the baseline habitat parcel’s value.</p>
         <p>The LPA, NCA and LNRS of a site or allocation is computed by lookup of the location within the area boundary - a rather slow operation without the help of a GIS index.</p>
         <p>The data is refreshed on a six-hourly basis.</p>
 
@@ -54,10 +55,7 @@ export default function About() {
 
         <h2>Development</h2>
         <p>This is a <ExternalLink href="https://nextjs.org/">Next.js</ExternalLink> project that displays Biodiversity Gain Sites data scraped from <ExternalLink href="https://environment.data.gov.uk/biodiversity-net-gain">https://environment.data.gov.uk/biodiversity-net-gain</ExternalLink>.</p>
-
-        <h2>What is Biodiversity Net Gain?</h2>
-        <p>Biodiversity Net Gain (BNG) represents a transformative shift in England's approach to development and environmental policy. Enacted under the Environment Act 2021, BNG is a legally binding framework designed to ensure that new developments, with limited exemptions, leave habitats for wildlife in a measurably better state than they were before construction. This is quantified through the use of a statutory biodiversity metric, which assigns a numerical value, or ‘biodiversity unit’, to habitats. Developers must achieve a minimum 10% gain in these units, a commitment that must be legally secured for at least 30 years.</p>
-        <p>The BNG process is not simply a spreadsheet calculation; it is heavily dependent on the professional expertise and judgment of ecologists. Ecologists conduct a thorough assessment of a site's baseline conditions, which includes desk studies and field surveys to map and classify habitat types. They must also assess a site's strategic significance.</p>
+  
       </main>
     </div>
   );
