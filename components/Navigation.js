5e9e32d98cbc1304260c6f0b56586fb49039f73c
Mark CD Ashdown <markcdashdown@Marks-Mac-mini.fritz.box>
2025-09-02 15:27:35 +0100

    modify the navigation component to that it is sticky...reformat

--- components/Navigation.js
+++ components/Navigation.js
@@ -16,13 +16,19 @@ export default function Navigation() {
 
   return (
     <nav className={styles.nav}>
-      <button className={styles.hamburger} onClick={toggleMenu}>
+      <button
+        className={styles.hamburger}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+        aria-expanded={isOpen}
+        aria-controls="navigation-menu"
+      >
         &#9776;
       </button>
-      <div className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
+      <div id="navigation-menu" className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
         <Link href="/about" className={styles.link} onClick={closeMenu}>About</Link>
-        <Link href="/sites" className={styles.link} onClick={closeMenu}>Site List</Link>
-        <Link href="/habitat-summary" className={styles.link} onClick={closeMenu}>Habitat Summary</Link>
+        <Link href="/sites" className={styles.link} onClick={closeMenu}>BGS Site List</Link>
+        <Link href="/habitat-summary" className={styles.link} onClick={closeMenu}>BGS Habitat Summary</Link>
       </div>
     </nav>
   );
