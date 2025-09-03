17e91bd6856dc2af958105446563577118a0578a
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-09-03 13:45:15 +0200

    added proxy for umami scripts & tracking

---
+++ pages/api/umami/api/send.js
@@ -0,0 +1,32 @@
+// pages/api/api/send.js
+export default async function handler(req, res) {
+  const umamiCollectUrl = 'http://umami-env-2.eba-kzd2jeqh.eu-central-1.elasticbeanstalk.com/api/send';
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  try {
+    const response = await fetch(umamiCollectUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': req.headers['user-agent'],
+        'x-forwarded-for': req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Failed to forward to umami: ${response.statusText}, ${errorText}`);
+    }
+
+    const data = await response.text();
+    res.status(response.status).send(data);
+
+  } catch (error) {
+    console.error('Error proxying umami collect request:', error);
+    res.status(500).json({ error: 'Failed to proxy request' });
+  }
+}
---
+++ pages/api/umami/script.js
@@ -0,0 +1,18 @@
+// pages/api/umami.js
+export default async function handler(req, res) {
+  const umamiScriptUrl = 'http://umami-env-2.eba-kzd2jeqh.eu-central-1.elasticbeanstalk.com/script.js';
+
+  try {
+    const scriptResponse = await fetch(umamiScriptUrl, { next: { revalidate: 86400 } });
+    if (!scriptResponse.ok) {
+      throw new Error(`Failed to fetch script: ${scriptResponse.statusText}`);
+    }
+    const scriptText = await scriptResponse.text();
+
+    res.setHeader('Content-Type', 'application/javascript');
+    res.status(200).send(scriptText);
+  } catch (error) {
+    console.error('Error proxying umami script:', error);
+    res.status(500).json({ error: 'Failed to proxy umami script' });
+  }
+}
