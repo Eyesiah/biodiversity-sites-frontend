e0c830e9280752342884a4f6be31136e73c95fa4
Mark CD Ashdown <markcdashdown@Marks-Mac-mini.fritz.box>
2025-09-09 09:28:53 +0100

    formatting changes

--- pages/habitat-analysis.js
+++ pages/habitat-analysis.js
@@ -285,14 +285,14 @@ export default function HabitatAnalysis({ areaAnalysis, hedgerowAnalysis, waterc
           className="linkButton"
           style={{ fontSize: '1.2rem', padding: '0.5rem 1rem', border: '1px solid #27ae60', borderRadius: '5px' }}
         >
-          View Improvement Habitats Chart
+          Improvement Habitats Chart
         </button>
         <button 
           onClick={() => window.open('/allocated-habitats', 'chartWindow', 'width=600,height=450')}
           className="linkButton"
           style={{ fontSize: '1.2rem', padding: '0.5rem 1rem', border: '1px solid #27ae60', borderRadius: '5px' }}
         >
-          View Allocated Habitats Chart
+          Allocated Habitats Chart
         </button>
       </div>
         <div className={styles.detailsGrid}>
