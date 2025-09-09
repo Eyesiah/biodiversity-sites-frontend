1fcc5b42dc1696b4785124b68c5a134c1964d5ad
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-09-09 15:23:57 +0200

    Added loading bar & spinner

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 function MyApp({ Component, pageProps }) {
   const umamiWebsiteId =
@@ -11,6 +14,25 @@ function MyApp({ Component, pageProps }) {
       : 'b718ac79-8ca2-494a-a398-47adf5e8188a';
 
   const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
 
   return (
     <>
