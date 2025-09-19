4a2f48032c57fefdc0683453ae6f171a81fa2605
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-09-19 13:36:10 +0200

    Removed dots from charts

--- pages/statistics.js
+++ pages/statistics.js
@@ -151,7 +151,7 @@ export default function StatisticsPage({ error, stats, siteAdditions }) {
             <Tooltip isAnimationActive={false} content={<CustomTooltip />} />
             <Legend />
             {dataKeys.map((dataKey, i) => (
-              <Line connectNulls key={dataKey} type="monotone" dataKey={dataKey} stroke={strokeColors[i]} name={names[i]} activeDot={{ r: 8 }} strokeWidth={3} />
+              <Line connectNulls key={dataKey} type="monotone" dataKey={dataKey} stroke={strokeColors[i]} name={names[i]} activeDot={{ r: 8 }} strokeWidth={3} dot={{ r: 0 }} />
             ))}
           </LineChart>
         </ResponsiveContainer>
