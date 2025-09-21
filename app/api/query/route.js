579176aac02be4fde502c1944006d9cf9aaffee4
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-09-21 19:34:09 +0200

    removed node cache

--- app/api/query/route.js
+++ app/api/query/route.js
@@ -2,13 +2,9 @@ import { fetchAllSites } from "@/lib/api";
 import OsGridRef from 'geodesy/osgridref.js';
 import { create } from 'xmlbuilder2';
 import { formatNumber } from '@/lib/format'
-import NodeCache from 'node-cache';
 import { NextResponse } from 'next/server';
 
 export const revalidate = 3600; // Re-generate page at most once per hour
-
-// Cache for 1 hour
-const cache = new NodeCache({ stdTTL: 3600 });
 
 export async function GET(request, { params }) {
   const { searchParams } = new URL(request.url);
@@ -20,50 +16,42 @@ export async function GET(request, { params }) {
   let dataElementName = ''
 
   if (mode === 'sites') {
-    const cacheKey = 'sites-data';
-    let cachedData = cache.get(cacheKey);
 
-    if (cachedData) {
-      data = cachedData;
-    } else {
-      const allSites = await fetchAllSites(true);
+    const allSites = await fetchAllSites(true);
 
-      // first get the easting/northings
-      allSites.forEach(s => {
-        var gridref = OsGridRef.parse(s.gridReference);
-        s.easting = gridref.easting;
-        s.northing = gridref.northing;
-      });
+    // first get the easting/northings
+    allSites.forEach(s => {
+      var gridref = OsGridRef.parse(s.gridReference);
+      s.easting = gridref.easting;
+      s.northing = gridref.northing;
+    });
 
-      // create the data array to return
-      data = allSites.map(s => ({
-        'reference-number': s.referenceNumber,
-        'responsible-body': s.responsibleBodies.join(', '),
-        'latitude': s.latitude,
-        'longitude': s.longitude,
-        'easting': s.easting,
-        'northing': s.northing,
-        'LPA': s.lpaName,
-        'NCA': s.ncaName,
-        'area-ha': s.siteSize,
-        'baseline-area-ha': formatNumber(s.habitats?.areas?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'baseline-hedgerow-km': formatNumber(s.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'baseline-watercourse-km': formatNumber(s.habitats?.watercourses?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'baseline-area-HU': formatNumber(s.habitats?.areas?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
-        'baseline-hedgerow-HU': formatNumber(s.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
-        'baseline-watercourse-HU': formatNumber(s.habitats?.watercourses?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
-        'improvement-area-ha': formatNumber(s.improvements?.areas?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'improvement-hedgerow-km': formatNumber(s.improvements?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'improvement-watercourse-km': formatNumber(s.improvements?.watercourses?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
-        'number-allocations': s.allocations?.length || 0,
-        'allocation-area-ha': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.areas?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
-        'allocation-hedgerow-km': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
-        'allocation-watercourse-km': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.watercourses?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
-      }));
+    // create the data array to return
+    data = allSites.map(s => ({
+      'reference-number': s.referenceNumber,
+      'responsible-body': s.responsibleBodies.join(', '),
+      'latitude': s.latitude,
+      'longitude': s.longitude,
+      'easting': s.easting,
+      'northing': s.northing,
+      'LPA': s.lpaName,
+      'NCA': s.ncaName,
+      'area-ha': s.siteSize,
+      'baseline-area-ha': formatNumber(s.habitats?.areas?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'baseline-hedgerow-km': formatNumber(s.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'baseline-watercourse-km': formatNumber(s.habitats?.watercourses?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'baseline-area-HU': formatNumber(s.habitats?.areas?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
+      'baseline-hedgerow-HU': formatNumber(s.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
+      'baseline-watercourse-HU': formatNumber(s.habitats?.watercourses?.reduce((acc, hab) => acc + hab.HUs, 0) || 0, 4),
+      'improvement-area-ha': formatNumber(s.improvements?.areas?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'improvement-hedgerow-km': formatNumber(s.improvements?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'improvement-watercourse-km': formatNumber(s.improvements?.watercourses?.reduce((acc, hab) => acc + hab.size, 0) || 0, 4),
+      'number-allocations': s.allocations?.length || 0,
+      'allocation-area-ha': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.areas?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
+      'allocation-hedgerow-km': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.hedgerows?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
+      'allocation-watercourse-km': formatNumber(s.allocations?.reduce((acc, alloc) => acc + alloc.habitats?.watercourses?.reduce((acc, hab) => acc + hab.size, 0), 0) || 0, 4),
+    }));
 
-      cache.set(cacheKey, data);
-    }
-    
     rootElementName = 'sites';
     dataElementName = 'site';
 
