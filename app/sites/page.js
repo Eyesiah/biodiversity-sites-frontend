d885971b1c31672a35eff930927f45a84eb4a4f4
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-09-21 17:41:52 +0200

    added revalidate to site list

--- app/sites/page.js
+++ app/sites/page.js
@@ -4,6 +4,9 @@ import { processSiteDataForIndex } from '@/lib/sites';
 import SiteListPageContent from './SiteListPageContent';
 import Footer from '@/components/Footer';
 
+// Revalidate this page at most once every hour (3600 seconds)
+export const revalidate = 3600;
+
 export default async function SiteListPage() {
   const allSites = await fetchAllSites(true);
   const { processedSites, summary } = processSiteDataForIndex(allSites);
