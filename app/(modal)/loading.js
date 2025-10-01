378678018146ab6b5df82a2497e66af5b3737a00
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-10-01 13:25:22 +0200

    Fix for missing loading circle

---
+++ app/(modal)/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+      <div className="loader"></div>
+    </div>
+  );
+}
