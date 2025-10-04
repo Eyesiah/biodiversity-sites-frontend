b803c76315b9cecb04166782ef17218e8d19b2b0
Mark CD shdown <mark.cd.ashdown@gmail.com>
2025-10-04 12:51:52 +0100

    add link to BTF in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
+import ExternalLink from '@/components/ExternalLink';
 export default function Footer({ lastUpdated }) {
   return (
     <footer style={{ textAlign: 'center', padding: '1rem', fontSize: '0.8rem', color: '#aaa', backgroundColor: '#282c34' }}>
-      {lastUpdated && (
+     
+     <p>Designed by <ExternalLink href="https://bristoltreeforum.org/"><b>Bristol Tree Forum</b></ExternalLink></p>
+
+     {lastUpdated && (  
         <p>
           Page last updated: {new Date(lastUpdated).toLocaleString('en-GB', { timeZone: 'UTC' })} UTC
         </p>
