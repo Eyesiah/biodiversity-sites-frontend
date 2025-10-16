3f807fb36b634e91a5f1538b8ab950710cffcdf6
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-10-16 15:57:20 +0200

    Navigation chakra conversion

--- components/ExternalLink.js
+++ components/ExternalLink.js
@@ -1,8 +1,8 @@
 // components/ExternalLink.js
 import React from 'react';
-import { Link as ChakraLink } from '@chakra-ui/react';
+import { Link as ChakraLink, Box } from '@chakra-ui/react';
 
-const ExternalLink = ({ href, children, className }) => {
+const ExternalLink = ({ href, children, className, showIcon = true }) => {
   return (
     <ChakraLink 
       href={href} 
@@ -10,10 +10,27 @@ const ExternalLink = ({ href, children, className }) => {
       target="_blank" 
       rel="noopener noreferrer"
       color="link"
-      textDecoration="underline"
+      textDecoration={showIcon ? "underline" : "none"}
       _hover={{ color: "linkHover" }}
+      display="inline-flex"
+      alignItems="center"
+      gap="0.25rem"
     >
       {children}
+      {showIcon && (
+        <Box
+          as="span"
+          display="inline-block"
+          width="12px"
+          height="12px"
+          marginLeft="0.25rem"
+          backgroundImage="url('/icons/external-link.svg')"
+          backgroundSize="contain"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          verticalAlign="middle"
+        />
+      )}
     </ChakraLink>
   );
 };
