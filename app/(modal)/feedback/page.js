9e1adab98471890a613e8f74b7ba32ca4f92ecc4
Mark CD shdown <mark.cd.ashdown@gmail.com>
2025-10-30 14:25:29 +0000

    correct missing escape code

--- app/(modal)/feedback/page.js
+++ app/(modal)/feedback/page.js
@@ -12,7 +12,7 @@ export default function Feedback() {
       <ProseContainer>
         <Heading as="h3" size="md">Feedback</Heading>
 
-        <Text>This website is still in beta, so we're interested to receive any feedback from users in the BNG/BGS community about what works for you.</Text>
+        <Text>This website is still in beta, so we&apos;re interested to receive any feedback from users in the BNG/BGS community about what works for you.</Text>
         <Text>Please send your feedback to: BGS_Suggestions@bristoltreeforum.org.</Text>
         <Text>If you have a bug to report or new feature suggestion, please email us or create a new issue on our <ExternalLink href="https://github.com/Eyesiah/biodiversity-sites-frontend/issues">GitHub page</ExternalLink>.</Text>
       </ProseContainer>
