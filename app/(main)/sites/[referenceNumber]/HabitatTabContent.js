55d0789a82c3332f81d536a7e2f90a09e3ad8ed2
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-11-09 22:20:51 +0100

    fixed heading errors

--- app/(main)/sites/[referenceNumber]/HabitatTabContent.js
+++ app/(main)/sites/[referenceNumber]/HabitatTabContent.js
@@ -3,7 +3,7 @@
 import { HabitatTable } from "@/components/data/HabitatsTable"
 import { ContentStack } from '@/components/styles/ContentStack'
 import { PrimaryCard, CardTitle } from '@/components/styles/PrimaryCard'
-import { Collapsible, Heading } from '@chakra-ui/react'
+import { Collapsible, Text } from '@chakra-ui/react'
 import { useState } from 'react'
 import SiteHabitatSankeyChart from "@/components/charts/SiteHabitatSankeyChart";
 
@@ -26,7 +26,7 @@ export default function HabitatTabContent({
       <SiteHabitatSankeyChart data={sankeyData} habitatType={habitatType} />
       <PrimaryCard>
         <CardTitle onClick={() => setImprovementOpen(!improvementOpen)}>
-          <Heading as="h2" size="lg">{`${habitatType} Improvements ${improvementOpen ? '▼' : '▶'}`}</Heading>            
+          <Text >{`${habitatType} Improvements ${improvementOpen ? '▼' : '▶'}`}</Text>            
         </CardTitle>
         <Collapsible.Root open={improvementOpen} onOpenChange={setImprovementOpen}>
           <Collapsible.Content>
@@ -42,7 +42,7 @@ export default function HabitatTabContent({
       </PrimaryCard>
       <PrimaryCard>
         <CardTitle onClick={() => setBaselineOpen(!baselineOpen)}>
-          <Heading as="h2" size="lg">{`Baseline ${habitatType} ${improvementOpen ? '▼' : '▶'}`}</Heading>
+          <Text >{`Baseline ${habitatType} ${improvementOpen ? '▼' : '▶'}`}</Text>
         </CardTitle>
         <Collapsible.Root open={baselineOpen} onOpenChange={setBaselineOpen}>
           <Collapsible.Content>
