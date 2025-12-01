aa580d04abc7a95219cbccc74faeb87204b6f4e6
Isaac Ashdown <isaac@maschinen-mensch.com>
2025-12-01 13:06:21 +0100

    Map bodies are dotted lines instead of filled areas

--- components/map/MapStyles.js
+++ components/map/MapStyles.js
@@ -1,6 +1,6 @@
 
-export const lsoaStyle = { color: '#f1c40f', weight: 3, opacity: 1, fillColor: '#f1c40f', fillOpacity: 0.2 };
-export const lnrsStyle = { color: '#4CAF50', weight: 2, opacity: 0.8, fillOpacity: 0.2 };
-export const ncaStyle = { color: '#8e44ad', weight: 2, opacity: 0.8, fillOpacity: 0.2 };
-export const lpaStyle = { color: '#282c34', weight: 2, opacity: 0.8, fillColor: '#282c34', fillOpacity: 0.3 };
-export const adjacentStyle = { color: '#FFC0CB', weight: 1, opacity: 0.7, fillOpacity: 0.5 }; // Pink
+export const lsoaStyle = { color: '#f1c40f', weight: 4, opacity: 1, fillOpacity: 0, dashArray: '5, 5' };
+export const lnrsStyle = { color: '#FF6600', weight: 4, opacity: 1, fillOpacity: 0, dashArray: '5, 5' };
+export const ncaStyle = { color: '#0066FF', weight: 4, opacity: 1, fillOpacity: 0, dashArray: '5, 5' };
+export const lpaStyle = { color: '#FF00FF', weight: 4, opacity: 1, fillOpacity: 0, dashArray: '5, 5' };
+export const adjacentStyle = { color: '#FF0000', weight: 4, opacity: 1, fillOpacity: 0, dashArray: '5, 5' };
