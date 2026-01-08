37acc51c30071fc16220e8c0a9b00812e61bf3ec
Mark CD shdown <mark.cd.ashdown@gmail.com>
2026-01-08 16:06:29 +0000

    Change bar chart colour

--- app/(main)/sites/SiteListPageContent.js
+++ app/(main)/sites/SiteListPageContent.js
@@ -123,7 +123,7 @@ const SiteSizeDistributionChart = ({sites}) => {
           <XAxis dataKey="name" label={{ value: 'Site Size Intervals', position: 'insideBottom', offset: -10, fill: '#36454F', fontWeight: 'bold', fontSize: '1.1rem' }} tick={{ fill: '#36454F' }} axisLine={{ stroke: 'black' }} />
           <YAxis label={{ value: 'Number of Sites', angle: -90, position: 'insideLeft', fill: '#36454F', fontWeight: 'bold', fontSize: '1.1rem' }} tick={{ fill: '#36454F' }} axisLine={{ stroke: 'black' }} />
           <Tooltip content={<CustomTooltip />} />
-          <Bar dataKey="count" fill="#4CAF50">
+          <Bar dataKey="count" fill="#9fd08eff">
             <LabelList />
           </Bar>
         </BarChart>
