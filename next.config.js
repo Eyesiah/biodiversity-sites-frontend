190c67aa4c46c77fb2012be896ab82adb64ac710
Isaac Ashdown <isaac@maschinen-mensch.com>
2026-03-02 15:33:59 +0100

    Increased static page generation time

--- next.config.js
+++ next.config.js
@@ -24,7 +24,10 @@ const nextConfig = {
   },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
-  },
+  },  
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 180,
   async redirects() {
     return [
       {
