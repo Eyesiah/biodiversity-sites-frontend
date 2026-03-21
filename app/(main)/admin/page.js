2329d2e2611f37d4322990ddeb932b555d9c74a5
Mark CD shdown <mark.cd.ashdown@gmail.com>
2026-03-21 12:38:22 +0000

    Change function getSiteNamesData() to getExtraSiteDetails()

--- app/(main)/admin/page.js
+++ app/(main)/admin/page.js
@@ -13,7 +13,7 @@ export const metadata = {
   description: 'Administrative functions for managing site data.'
 };
 
-async function getSiteNamesData() {
+async function getExtraSiteDetails() {
   try {
     const client = await clientPromise;
     const db = client.db(MONGODB_DATABASE_NAME);
@@ -45,7 +45,7 @@ async function getSiteNamesData() {
 export default async function AdminPage({}) {
   // Fetch all sites to get reference numbers
   const sites = await fetchAllSites();
-  const nameMap = await getSiteNamesData();
+  const nameMap = await getExtraSiteDetails();
 
   // Create reference number options with names
   const referenceOptions = sites.map(site => {
