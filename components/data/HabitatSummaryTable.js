4664559d8fa1318ff9f74295c4d398f35f25e546
Mark CD shdown <mark.cd.ashdown@gmail.com>
2026-03-27 12:03:41 +0000

    Break out improvements data to report created and enhanced habitat size and HUs and reorder the summary table

--- components/data/HabitatSummaryTable.js
+++ components/data/HabitatSummaryTable.js
@@ -37,6 +37,26 @@ export const HabitatSummaryTable = ({ site }) => {
   const improvementHedgerow = (improvements.hedgerows || []).reduce((acc, h) => acc + h.size, 0);
   const improvementWatercourse = (improvements.watercourses || []).reduce((acc, h) => acc + h.size, 0);
 
+  const createdArea = (improvements.areas || []).reduce((acc, h) => acc + (h.createdSize || 0), 0);
+  const createdTrees = (improvements.trees || []).reduce((acc, h) => acc + (h.createdSize || 0), 0);
+  const createdHedgerow = (improvements.hedgerows || []).reduce((acc, h) => acc + (h.createdSize || 0), 0);
+  const createdWatercourse = (improvements.watercourses || []).reduce((acc, h) => acc + (h.createdSize || 0), 0);
+
+  const enhancedArea = (improvements.areas || []).reduce((acc, h) => acc + (h.enhancedSize || 0), 0);
+  const enhancedTrees = (improvements.trees || []).reduce((acc, h) => acc + (h.enhancedSize || 0), 0);
+  const enhancedHedgerow = (improvements.hedgerows || []).reduce((acc, h) => acc + (h.enhancedSize || 0), 0);
+  const enhancedWatercourse = (improvements.watercourses || []).reduce((acc, h) => acc + (h.enhancedSize || 0), 0);
+
+  const createdAreaHUs = (improvements.areas || []).reduce((acc, h) => acc + (h.createdHUs || 0), 0);
+  const createdTreesHUs = (improvements.trees || []).reduce((acc, h) => acc + (h.createdHUs || 0), 0);
+  const createdHedgerowHUs = (improvements.hedgerows || []).reduce((acc, h) => acc + (h.createdHUs || 0), 0);
+  const createdWatercourseHUs = (improvements.watercourses || []).reduce((acc, h) => acc + (h.createdHUs || 0), 0);
+
+  const enhancedAreaHUs = (improvements.areas || []).reduce((acc, h) => acc + (h.enhancedHUs || 0), 0);
+  const enhancedTreesHUs = (improvements.trees || []).reduce((acc, h) => acc + (h.enhancedHUs || 0), 0);
+  const enhancedHedgerowHUs = (improvements.hedgerows || []).reduce((acc, h) => acc + (h.enhancedHUs || 0), 0);
+  const enhancedWatercourseHUs = (improvements.watercourses || []).reduce((acc, h) => acc + (h.enhancedHUs || 0), 0);
+
   const improvementAreaHUs = (improvements.areas || []).reduce((acc, h) => acc + h.HUs, 0);
   const improvementTreesHUs = (improvements.trees || []).reduce((acc, h) => acc + h.HUs, 0);
   const improvementHedgerowHUs = (improvements.hedgerows || []).reduce((acc, h) => acc + h.HUs, 0);
@@ -90,21 +110,28 @@ export const HabitatSummaryTable = ({ site }) => {
   const headerFontSize = '14px';
 
   return (
-    <Box overflowX="auto">
+    <Box overflowX="auto" display="flex" justifyContent="center">
       <DataTable.Root width="auto" margin="0">
         <DataTable.Header>
           <DataTable.Row>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Habitat'>Habitat</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}># <GlossaryTooltip term='Parcel'>Parcels</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Baseline habitat'>Baseline</GlossaryTooltip><br /><GlossaryTooltip term='Size'>Size</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Baseline habitat'>Baseline</GlossaryTooltip><br /><GlossaryTooltip term='Habitat Unit (HU)'>HUs</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Retained habitat'>Retained</GlossaryTooltip><br /><GlossaryTooltip term='Size'>Size</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Improvements</GlossaryTooltip><br /><GlossaryTooltip term='Size'>Size</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Improvements</GlossaryTooltip><br /><GlossaryTooltip term='Habitat Unit (HU)'>HUs</GlossaryTooltip></DataTable.ColumnHeader>
-            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Improvements</GlossaryTooltip><br /><GlossaryTooltip term='Habitat Unit (HU)'>HU</GlossaryTooltip> <GlossaryTooltip term='HU Gain'>Gain</GlossaryTooltip></DataTable.ColumnHeader>
-            {hasAllocs && <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Allocation'>Allocations</GlossaryTooltip><br /><GlossaryTooltip term='Size'>Size</GlossaryTooltip></DataTable.ColumnHeader>}
+            <DataTable.ColumnHeader fontSize={headerFontSize} rowSpan={2}><GlossaryTooltip term='Habitat'>Habitat</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize} rowSpan={2}># <GlossaryTooltip term='Parcel'>Parcels</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize} colSpan={4 + (hasAllocs ? 2 : 0)} textAlign="center">Size</DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize} colSpan={5 + (hasAllocHUs ? 1 : 0)} textAlign="center">Habitat Units</DataTable.ColumnHeader>
+          </DataTable.Row>
+          <DataTable.Row>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Baseline habitat'>Baseline</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Created</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Enhanced</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Improvements</GlossaryTooltip></DataTable.ColumnHeader>
+            {hasAllocs && <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Allocation'>Allocations</GlossaryTooltip></DataTable.ColumnHeader>}
             {hasAllocs && <DataTable.ColumnHeader fontSize={headerFontSize}>% <GlossaryTooltip term='Allocation'>Allocated</GlossaryTooltip></DataTable.ColumnHeader>}
-            {hasAllocHUs && <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Allocation'>Allocations</GlossaryTooltip><br /><GlossaryTooltip term='Habitat Unit (HU)'>HUs</GlossaryTooltip></DataTable.ColumnHeader>}
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Baseline habitat'>Baseline</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Created</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Enhanced</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Improvement habitat'>Improvements</GlossaryTooltip></DataTable.ColumnHeader>
+            <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='HU Gain'>HU Gain</GlossaryTooltip></DataTable.ColumnHeader>
+            {hasAllocHUs && <DataTable.ColumnHeader fontSize={headerFontSize}><GlossaryTooltip term='Allocation'>Allocations</GlossaryTooltip></DataTable.ColumnHeader>}
           </DataTable.Row>
         </DataTable.Header>
         <DataTable.Body>
@@ -112,19 +139,16 @@ export const HabitatSummaryTable = ({ site }) => {
             <DataTable.Cell>Areas</DataTable.Cell>
             <DataTable.NumericCell>{formatNumber(baselineAreaParcels, 0)}</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(baselineArea, 2)} ha</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(baselineAreaHUs)}</DataTable.NumericCell>
-            <DataTable.NumericCell color={(() => {
-              const retainedArea = baselineArea - improvementArea;
-              return Math.abs(retainedArea) < 0.005 ? 0 : retainedArea;
-            })() < 0 ? "red.500" : undefined}>{formatNumber((() => {
-              const retainedArea = baselineArea - improvementArea;
-              return Math.abs(retainedArea) < 0.005 ? 0 : retainedArea;
-            })(), 2)} ha</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdArea, 2)} ha</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedArea, 2)} ha</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(improvementArea, 2)} ha</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementAreaHUs, 2)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementAreaHUGain, 2)}</DataTable.NumericCell>
             {hasAllocs && <DataTable.NumericCell>{formatNumber(allocationArea, 2)} ha</DataTable.NumericCell>}
             {hasAllocs && <DataTable.NumericCell>{improvementArea > 0 ? formatNumber((allocationArea / improvementArea) * 100, 2) + '%' : 'N/A'}</DataTable.NumericCell>}
+            <DataTable.NumericCell>{formatNumber(baselineAreaHUs)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdAreaHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedAreaHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementAreaHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementAreaHUGain, 2)}</DataTable.NumericCell>
             {hasAllocHUs && <DataTable.NumericCell>{formatNumber(allocationAreaHUs)}</DataTable.NumericCell>}
           </DataTable.Row>}
           {hasIndividualTrees && <DataTable.Row>
@@ -133,47 +157,58 @@ export const HabitatSummaryTable = ({ site }) => {
             <DataTable.NumericCell>
               {formatTreeCountWithTooltip(baselineIndividualTrees)}
             </DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(baselineIndividualTreesHUs)}</DataTable.NumericCell>
             <DataTable.NumericCell>
-              {formatTreeCountWithTooltip(Math.max(0, baselineIndividualTrees - improvementTrees))}
+              {formatTreeCountWithTooltip(createdTrees)}
+            </DataTable.NumericCell>
+            <DataTable.NumericCell>
+              {formatTreeCountWithTooltip(enhancedTrees)}
             </DataTable.NumericCell>
             <DataTable.NumericCell>
               {formatTreeCountWithTooltip(improvementTrees)}
             </DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementTreesHUs, 2)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementTreesHUGain, 2)}</DataTable.NumericCell>
             {hasAllocs && (
               <DataTable.NumericCell>
                 {formatTreeCountWithTooltip(allocationIndividualTrees)}
               </DataTable.NumericCell>
             )}
             {hasAllocs && <DataTable.NumericCell>{improvementTrees > 0 ? formatNumber((allocationIndividualTrees / improvementTrees) * 100, 2) + '%' : 'N/A'}</DataTable.NumericCell>}
+            <DataTable.NumericCell>{formatNumber(baselineIndividualTreesHUs)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdTreesHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedTreesHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementTreesHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementTreesHUGain, 2)}</DataTable.NumericCell>
             {hasAllocHUs && <DataTable.NumericCell>{formatNumber(0)}</DataTable.NumericCell>}
           </DataTable.Row>}
           {hasHedgerow && <DataTable.Row>
             <DataTable.Cell>Hedgerows</DataTable.Cell>
             <DataTable.NumericCell>{formatNumber(baselineHedgerowParcels, 0)}</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(baselineHedgerow, 2)} km</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(baselineHedgerowHUs)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(Math.max(0, baselineHedgerow - improvementHedgerow), 2)} km</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdHedgerow, 2)} km</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedHedgerow, 2)} km</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(improvementHedgerow, 2)} km</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementHedgerowHUs, 2)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementHedgerowHUGain, 2)}</DataTable.NumericCell>
             {hasAllocs && <DataTable.NumericCell>{formatNumber(allocationHedgerow, 2)} km</DataTable.NumericCell>}
             {hasAllocs && <DataTable.NumericCell>{improvementHedgerow > 0 ? formatNumber((allocationHedgerow / improvementHedgerow) * 100, 2) + '%' : 'N/A'}</DataTable.NumericCell>}
+            <DataTable.NumericCell>{formatNumber(baselineHedgerowHUs)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdHedgerowHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedHedgerowHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementHedgerowHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementHedgerowHUGain, 2)}</DataTable.NumericCell>
             {hasAllocHUs && <DataTable.NumericCell>{formatNumber(allocationHedgerowHUs)}</DataTable.NumericCell>}
           </DataTable.Row>}
           {hasWatercourse && <DataTable.Row>
             <DataTable.Cell>Watercourses</DataTable.Cell>
             <DataTable.NumericCell>{formatNumber(baselineWatercourseParcels, 0)}</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(baselineWatercourse, 2)} km</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(baselineWatercourseHUs)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(Math.max(0, baselineWatercourse - improvementWatercourse), 2)} km</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdWatercourse, 2)} km</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedWatercourse, 2)} km</DataTable.NumericCell>
             <DataTable.NumericCell>{formatNumber(improvementWatercourse, 2)} km</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementWatercourseHUs, 2)}</DataTable.NumericCell>
-            <DataTable.NumericCell>{formatNumber(improvementWatercourseHUGain, 2)}</DataTable.NumericCell>
             {hasAllocs && <DataTable.NumericCell>{formatNumber(allocationWatercourse, 2)} km</DataTable.NumericCell>}
             {hasAllocs && <DataTable.NumericCell>{improvementWatercourse > 0 ? formatNumber((allocationWatercourse / improvementWatercourse) * 100, 2) + '%' : 'N/A'}</DataTable.NumericCell>}
+            <DataTable.NumericCell>{formatNumber(baselineWatercourseHUs)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(createdWatercourseHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(enhancedWatercourseHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementWatercourseHUs, 2)}</DataTable.NumericCell>
+            <DataTable.NumericCell>{formatNumber(improvementWatercourseHUGain, 2)}</DataTable.NumericCell>
             {hasAllocHUs && <DataTable.NumericCell>{formatNumber(allocationWatercourseHUs)}</DataTable.NumericCell>}
           </DataTable.Row>}
         </DataTable.Body>
