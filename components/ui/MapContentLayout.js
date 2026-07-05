453b642c2c154bd9000ccb61504425aead6d3d28
Mark CD shdown <mark.cd.ashdown@gmail.com>
2026-07-05 22:31:26 +0100

    Change allocation maps layout

--- components/ui/MapContentLayout.js
+++ components/ui/MapContentLayout.js
@@ -3,7 +3,7 @@ import { useIsMobile } from '@/lib/hooks.js';
 import { Box, Flex } from '@chakra-ui/react';
 import { NAV_HEIGHT } from '@/config';
 
-const MapContentLayout = ({ map, content }) => {
+const MapContentLayout = ({ map, content, hideMap = false }) => {
   const isMobile = useIsMobile();
   const [hasMounted, setHasMounted] = useState(false);
 
@@ -17,7 +17,7 @@ const MapContentLayout = ({ map, content }) => {
       width="100%"
       minHeight={{ base: "auto", md: "100vh" }}
     >
-      {!isMobile &&
+      {!isMobile && !hideMap &&
         <Box
           flex="1 1 33%"
           marginRight="1rem"
@@ -30,7 +30,7 @@ const MapContentLayout = ({ map, content }) => {
         </Box>
       }
       <Box
-        flex={{ base: "1 1 100%", md: "1 1 67%" }}
+        flex={{ base: "1 1 100%", md: hideMap ? "1 1 100%" : "1 1 67%" }}
         py="1rem"
         overflowY={{ base: "visible", md: "auto" }}
       >
